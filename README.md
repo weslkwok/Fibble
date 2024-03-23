02a81e2a56b33240689b0304b0c51df53ebbbec9
weslkwok <63258448+weslkwok@users.noreply.github.com>
2024-03-22 18:30:31 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-use command "npm run both" while in frontend folder to start both the frontend and backend
+# Fibble Fixer
+## CSC 481: Knowledge Based Systems, Cal Poly SLO
+### Class instructed by Dr. Rodrigo Canaan
 
-all the logic for fibble guesses should be done in the Python backend
+Fibble fixer is a [Fibble](https://fibble.xyz/) solver inspired by [3Blue1Brown's Youtube Video on Solving Wordle via Information Theory](https://www.youtube.com/watch?v=v68zYyaEmEA).
+
+This package requires the use of the following dependencies:
+
+* [Flask](https://flask.palletsprojects.com/en/3.0.x/installation/)
+* NPM
+* flask_sqlalchemy (pip install it)
+* flask_cors (pip install it)
+
+use command "npm run both" while in frontend folder to start both the frontend and backend. This requires the use of Concurrency. Alternatively, you can run "python3 app.y" in the backend folder, and "npm run start" in the frontend folder.
+
+all the logic for fibble guesses should be done in the Python backend.
+
+To validate our results, start up the frontend / backend and use the webpage to generate a bunch of guesses. The results will be logged onto the console on the backend.
--- backend/app.py
+++ backend/app.py
@@ -9,19 +9,59 @@ from wordle import generate_guesses_wordle
 app = Flask(__name__)
 CORS(app)
 
+global fibble_wins
+global fibble_losses
+global fibble_guesses
+
+global wordle_wins
+global wordle_losses
+global wordle_guesses
+
+fibble_wins = 0
+fibble_losses = 0
+fibble_guesses = 0
+
+wordle_wins = 0
+wordle_losses = 0
+wordle_guesses = 0
 
 # returns guesses for each goal given
 @app.route('/guess', methods=['POST'])
 def get_guesses():
+    global fibble_wins
+    global fibble_losses
+    global fibble_guesses
+
+    global wordle_wins
+    global wordle_losses
+    global wordle_guesses
     rounds = []
     if not request.json['fibble']:
         for goal in request.json['goals']:
             rounds.append([guess.to_dict() for guess in generate_guesses_wordle(goal, request.json['fibble'])])
+            if len(rounds[0]) > 9:
+                wordle_losses += 1
+            else:
+                wordle_wins += 1
+            wordle_guesses += len(rounds[0])
     else:
         for goal in request.json['goals']:
-            print(request.json)
             rounds.append([guess.to_dict() for guess in generate_guesses_fibble(goal, request.json['fibble'])])
-        
+            print(rounds)
+            if len(rounds[0]) > 9:
+                fibble_losses += 1
+            else:
+                fibble_wins += 1
+            fibble_guesses += len(rounds[0])
+    
+    print("WORDLE LOSSES:", wordle_losses)
+    print("WORDLE WINS:", wordle_wins)
+    if (wordle_losses + wordle_wins):
+        print("WORDLE TURNS:", (wordle_guesses / (wordle_losses + wordle_wins)))
+    print("FIBBLE LOSSES:", fibble_losses)
+    print("FIBBLE WINS:", fibble_wins)
+    if (fibble_losses + fibble_wins):
+        print("FIBBLE TURNS:", (fibble_guesses / (fibble_wins + fibble_losses)))
     return jsonify(rounds)
 
 
